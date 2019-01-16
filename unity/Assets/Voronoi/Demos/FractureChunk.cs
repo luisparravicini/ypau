c59e75906bde3f2360e6a5ed7b7f1d204dcfd985
Luis Parravicini <lparravi@gmail.com>
2019-01-16 14:19:25 -0300

    new scene for maps based on demo

--- unity/Assets/Voronoi/Demos/FractureChunk.cs
+++ unity/Assets/Voronoi/Demos/FractureChunk.cs
@@ -3,45 +3,56 @@ using System.Collections;
 using System.Collections.Generic;
 
 using Voronoi;
+using System;
 
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
 [RequireComponent(typeof(MeshCollider))]
-public class FractureChunk : MonoBehaviour 
+public class FractureChunk : MonoBehaviour
 {
-	public Material material;
+    public Material material;
 
-	private Mesh mesh;
-	
-	public void CreateFanMesh(Cell cell)
-	{
+    private Mesh mesh;
+    private Renderer _meshRenderer;
+
+    private Renderer meshRenderer
+    {
+        get
+        {
+            if (_meshRenderer == null)
+                _meshRenderer = GetComponent<Renderer>();
+            return _meshRenderer;
+        }
+    }
+
+    public void CreateFanMesh(Cell cell)
+    {
         if (cell.halfEdges.Count > 0)
         {
-			GetComponent<MeshFilter>().sharedMesh = mesh = new Mesh();
-			mesh.name = "Chunk " + cell.site.id;
-			
-			Vector3[] vertices = new Vector3[cell.halfEdges.Count + 1];
-			int[] triangles = new int[(cell.halfEdges.Count + 0) * 3];
-			
-			vertices[0] = cell.site.ToVector3() - transform.position;
-			triangles[0] = 0;
-			for (int v = 1, t = 1; v < vertices.Length; v++, t += 3)
-			{
-				vertices[v] = cell.halfEdges[v-1].GetStartPoint().ToVector3() - transform.position;
-				triangles[t] = v;
-				triangles[t + 1] = v + 1;
-			}
-			triangles[triangles.Length - 1] = 1;
-			
-			mesh.vertices = vertices;
-			mesh.triangles = triangles;
-			mesh.RecalculateBounds();
+            GetComponent<MeshFilter>().sharedMesh = mesh = new Mesh();
+            mesh.name = "Chunk " + cell.site.id;
+
+            Vector3[] vertices = new Vector3[cell.halfEdges.Count + 1];
+            int[] triangles = new int[(cell.halfEdges.Count + 0) * 3];
+
+            vertices[0] = cell.site.ToVector3() - transform.position;
+            triangles[0] = 0;
+            for (int v = 1, t = 1; v < vertices.Length; v++, t += 3)
+            {
+                vertices[v] = cell.halfEdges[v - 1].GetStartPoint().ToVector3() - transform.position;
+                triangles[t] = v;
+                triangles[t + 1] = v + 1;
+            }
+            triangles[triangles.Length - 1] = 1;
+
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
+            mesh.RecalculateBounds();
 
             GetComponent<MeshCollider>().sharedMesh = mesh;
-			
-			GetComponent<Renderer>().sharedMaterial = material;
-		}
-	}
+            meshRenderer.sharedMaterial = material;
+        }
+    }
 
     public void CreateStipMesh(Cell cell)
     {
@@ -67,7 +78,12 @@ public class FractureChunk : MonoBehaviour
             mesh.triangles = triangles;
             mesh.RecalculateBounds();
 
-            GetComponent<Renderer>().sharedMaterial = material;
+            meshRenderer.sharedMaterial = material;
         }
+    }
+
+    internal void SetColor(Color c)
+    {
+        meshRenderer.material.color = c;
     }
 }
